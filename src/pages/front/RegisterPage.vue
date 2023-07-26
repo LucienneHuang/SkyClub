7198bd9a74c531144334295234031379bdaf79db
LucienneHuang <q22641949@gmail.com>
2023-07-27 06:45:33 +0800

    進度

--- src/pages/front/RegisterPage.vue
+++ src/pages/front/RegisterPage.vue
@@ -1,37 +1,43 @@
 <template>
   <div class="container">
-    <div class="row justify-center items-center">
-      <q-card class="my-card" flat bordered>
-      <q-card-section horizontal>
-        <q-card-section>
+    <div class="row justify-center items-center" style="height: 100%;">
+      <!-- <div class="glass flex col-12 justify-center" style="width: 80%;height: 60%;">
+        <div class="text-h4 q-mt-sm">註冊會員</div>
+        <q-input  label="Standard" />
+      </div> -->
+      <q-card class="login">
 
-        </q-card-section>
-        <q-card-section>
-
-        </q-card-section>
-      </q-card-section>
-    </q-card>
-      </div>
+      </q-card>
     </div>
+  </div>
 </template>
 <style scoped>
 .container {
   width: 100vw;
   height: 100vh;
-  .left{
-    display: none;
-  }
-  .right{
+  background: url('../../assets/background_register.jpg') no-repeat center;
+  background-size: cover;
+
+  .login{
     width: 80%;
+    height: 60%;
+    background: transparent;
+    backdrop-filter: blur(10px);
+  }
+  .text-h4{
+    font-weight: 600;
+    color: white;
   }
 }
 
 @media(min-width:768px){
-  .left{
-    display: block;
-  }
+
 }
 
   @media(min-width:1200px){
 }
 </style>
+
+<script setup>
+
+</script>
