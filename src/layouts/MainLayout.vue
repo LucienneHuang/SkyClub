c11ccb27147b97580cef9290fa764b5ae719c82e
LucienneHuang <q22641949@gmail.com>
2023-07-27 14:08:35 +0800

    註冊、登入畫面

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,7 @@
 <template>
   <q-layout view="hhh Lpr fff">
     <q-header class="text-white " height-hint="98">
+      <!-- 上層的 bar -->
       <q-toolbar class="row q-mt-md">
         <q-btn class="menuBtn" dense flat round icon="menu" @click="toggleLeftDrawer" />
         <q-toolbar-title class="col-md-2">
@@ -23,50 +24,42 @@
         </q-tabs>
         <!-- 右側 -->
         <!-- 之後再刻 遊客/會員/管理員 -->
-        <div>
-          <q-btn-dropdown color="primary" label="設定">
-            <q-list>
-              <q-item clickable v-close-popup >
-                <q-item-section>
-                  <q-item-label>遊客</q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-separator inset />
-              <q-item clickable v-close-popup to="/register">
-                <q-item-section>
-                  <q-item-label>註冊</q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-item clickable v-close-popup to="/login">
-                <q-item-section>
-                  <q-item-label>登入</q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-item clickable v-close-popup to="/member">
-                <q-item-section>
-                  <q-item-label>會員後台</q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-item clickable v-close-popup to="admin">
-                <q-item-section>
-                  <q-item-label>管理後台</q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-separator inset />
-              <q-item clickable v-close-popup to="/">
-                <q-item-section>
-                  <q-item-label>回首頁</q-item-label>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-btn-dropdown>
-        </div>
+        <q-btn class="q-ml-auto" flat round dense>
+        <q-avatar>
+          <img src="https://source.boringavatars.com/beam/250/Maria%20Mitchell?colors=264653,2a9d8f,e9c46a,f4a261,e76f51">
+        </q-avatar>
+        <q-menu square class="bg-primary text-center">
+          <q-list dense>
+            <q-item clickable>
+              <q-item-section >USER</q-item-section>
+            </q-item>
+            <q-separator inset />
+            <q-item clickable to="/admin">
+              <q-item-section>管理後台</q-item-section>
+            </q-item>
+            <q-item clickable to="/member">
+              <q-item-section>會員後台</q-item-section>
+            </q-item>
+            <q-item clickable to="/register">
+              <q-item-section>註冊</q-item-section>
+            </q-item>
+            <q-item clickable to="/login">
+              <q-item-section>登入</q-item-section>
+            </q-item>
+            <q-separator inset />
+            <q-item clickable to="/">
+              <q-item-section>回首頁</q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
       </q-toolbar>
+      <!-- 波浪背景 -->
       <div class="deco"></div>
     </q-header>
-
+    <!-- 手機螢幕才會有的側邊欄 -->
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered behavior="mobile">
-      <!-- drawer content -->
+      <!-- 之後藥補的側邊攔內容 -->
     </q-drawer>
 
     <q-page-container>
@@ -86,6 +79,75 @@
   </q-layout>
 </template>
 
+<style lang="scss" scoped>
+
+.q-header{
+  width: 100vw;
+  height: 20rem;
+  background-image: url('../assets/front.png');
+  background-repeat: no-repeat;
+  background-size:  cover;
+  .q-toolbar{
+    position: fixed;
+  }
+  .q-tab__label{
+    font-size: 18px;
+  }
+  .q-tab{
+  display: none;
+  }
+  .deco{
+    width: 100%;
+    height: 8rem;
+    position: relative;
+    top: 12rem;
+    background: url('../assets/deco.png') repeat-x center/cover;
+    &::before{
+      content: "";
+      width: 100%;
+      height: 8rem;
+      display: block;
+      position: relative;
+      top: -.5rem;
+      background: url('../assets/deco.png') repeat-x center/cover;
+      opacity: 0.5;
+    }
+  }
+}
+@media(min-width:768px){
+.q-header{
+  height: 32rem;
+  .menuBtn{
+    display: none;
+  }
+  .q-tab{
+    display: block;
+  }
+  .deco{
+    height: 9rem;
+    top: 23.5rem;
+    &::before{
+      height: 9rem;
+    }
+  }
+}
+}
+
+@media(min-width:1200px){
+  .q-header{
+    height: 50rem;
+    .deco{
+    height: 16rem;
+    top: 34.5rem;
+    &::before{
+      height: 16rem;
+    }
+  }
+
+}
+}
+</style>
+
 <script>
 import { ref } from 'vue'
 
@@ -102,71 +164,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-
-.q-header{
-  width: 100%;
-  height: 300px;
-  background-image: url('../assets/front.png');
-  background-repeat: no-repeat;
-  background-size:  cover;
-  .q-tab__label{
-    font-size: 18px;
-  }
-  .q-tab{
-  display: none;
-  }
-  .deco{
-    width: 100%;
-    height: 90px;
-    position: relative;
-    top: 145px;
-    background: url('../assets/deco.png') repeat-x center/cover;
-    &::before{
-      content: "";
-      width: 100%;
-      height: 90px;
-      display: block;
-      position: relative;
-      top: -8px;
-      background: url('../assets/deco.png') repeat-x center/cover;
-      opacity: 0.5;
-    }
-  }
-}
-@media(min-width:768px){
-.q-header{
-  height: 500px;
-  .menuBtn{
-    display: none;
-  }
-  .q-tab{
-    display: block;
-  }
-  .deco{
-    height: 150px;
-    top: 270px;
-    &::before{
-      height: 150px;
-      top: -10px;
-    }
-  }
-}
-}
-
-  @media(min-width:1200px){
-.q-header{
-  height: 800px;
-  .deco{
-    height: 250px;
-    top: 480px;
-    &::before{
-      height: 250px;
-      top: -10px;
-    }
-  }
-
-}
-}
-</style>
--- src/pages/front/RegisterPage.vue
+++ src/pages/front/RegisterPage.vue
@@ -1,37 +1,101 @@
 <template>
   <div class="container">
-    <div class="row justify-center items-center">
-      <q-card class="my-card" flat bordered>
-      <q-card-section horizontal>
-        <q-card-section>
-
-        </q-card-section>
-        <q-card-section>
-
-        </q-card-section>
-      </q-card-section>
-    </q-card>
+    <q-toolbar class="text-white q-px-lg" style="height: 50px;">
+      <q-btn class="q-ml-auto" flat round dense>
+        <q-avatar>
+          <img src="https://source.boringavatars.com/beam/250/Maria%20Mitchell?colors=264653,2a9d8f,e9c46a,f4a261,e76f51">
+        </q-avatar>
+        <q-menu square class="bg-primary text-center">
+          <q-list dense>
+            <q-item clickable>
+              <q-item-section >USER</q-item-section>
+            </q-item>
+            <q-separator inset />
+            <q-item clickable to="/admin">
+              <q-item-section>管理後台</q-item-section>
+            </q-item>
+            <q-item clickable to="/member">
+              <q-item-section>會員後台</q-item-section>
+            </q-item>
+            <q-item clickable to="/register">
+              <q-item-section>註冊</q-item-section>
+            </q-item>
+            <q-item clickable to="/login">
+              <q-item-section>登入</q-item-section>
+            </q-item>
+            <q-separator inset />
+            <q-item clickable to="/">
+              <q-item-section>回首頁</q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
+    </q-toolbar>
+    <div class="row flex-center">
+      <div class="rg q-card q-pa-xs">
+        <div class="q-card-section q-px-lg">
+          <div class="title text-center text-h4 q-py-md">註冊會員</div>
+          <q-input class="q-mb-xs" v-model="emailText" label="Email" hint="請輸入信箱" hide-hint filled dense clearable hide-bottom-space clear-icon="backspace">
+            <template #prepend>
+              <q-icon name="mail"></q-icon>
+            </template>
+          </q-input>
+          <q-input class="q-mb-xs" type="password" label="Password" hint="請輸入密碼" hide-hint filled dense hide-bottom-space>
+            <template #prepend>
+              <q-icon name="vpn_key"></q-icon>
+            </template>
+          </q-input>
+          <q-input class="q-mb-xs" type="password" label="Confirm Password" hint="確認信箱" hide-hint filled dense hide-bottom-space>
+            <template #prepend>
+              <q-icon name="vpn_key"></q-icon>
+            </template>
+          </q-input>
+        </div>
+        <q-card-actions class="q-mt-md q-px-lg" vertical>
+          <q-btn flat label="register" class="rg-btn" color="white" style="background: #A6D8D4;font-family: 'Playfair Display', serif;"></q-btn>
+          <q-card-actions align="center">
+            <q-btn label="快速註冊" flat></q-btn>
+            <q-btn label="登入會員" to="/login" flat></q-btn>
+          </q-card-actions>
+        </q-card-actions>
       </div>
     </div>
+  </div>
 </template>
 <style scoped>
 .container {
   width: 100vw;
   height: 100vh;
-  .left{
-    display: none;
+  background: url('../../assets/register_bg.jpg') no-repeat center/cover;
+  .row{
+    width:100%;
+    height: 100%;
   }
-  .right{
-    width: 80%;
+  .rg{
+    width: 20rem;
+    height: 20rem;
+    background: transparent;
+    backdrop-filter: blur(15px);
+    .rg-btn{
+      font-size: 1rem;
+    }
+    .title{
+      font-weight: 600;
+      color: white;
+      text-shadow: black 0 0 0.2em;
+    }
   }
+
 }
 
 @media(min-width:768px){
-  .left{
-    display: block;
-  }
+
 }
 
   @media(min-width:1200px){
 }
 </style>
+<script setup>
+import { ref } from 'vue'
+const emailText = ref('')
+</script>
