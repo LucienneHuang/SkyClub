eab8f2f0912796a5f8970b703e102277270fb012
LucienneHuang <q22641949@gmail.com>
2023-07-28 13:54:46 +0800

    新增notify的plugins

--- quasar.config.js
+++ quasar.config.js
@@ -90,7 +90,11 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
-      config: {},
+      config: { // optional (v0.17+)
+        notify: {
+          // Notify defaults
+        }
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -103,7 +107,7 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ['Notify']
     },
 
     // animations: 'all', // --- includes all animations
