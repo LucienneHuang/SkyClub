0d10f4de34d332af636a2013c85fba1c8e995efb
LucienneHuang <q22641949@gmail.com>
2023-07-31 16:05:52 +0800

    每個 pages  載入標題 & beforeeach

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import { route } from 'quasar/wrappers'
-import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
+// START_LOCATION 是進到頁面的第一次跳轉
+// 假設他第一次進到你的網頁，他的網址是 /login，就代表他的 START_LOCATION 是 login
+// 就是第一次路由導航的位置
+// 可以判斷他是不是第一次進這個網頁
+import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory, START_LOCATION } from 'vue-router'
 import routes from './routes'
+import { useUserStore } from 'src/stores/user'
 
 /*
  * If not building with SSR mode, you can
@@ -25,9 +30,38 @@ export default route(function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE)
   })
+  // to 去哪裡
+  // from 從哪裡來
 
   Router.afterEach((to, from) => {
     document.title = to.meta.title
+  })
+  // next 如果 okay 導去下一頁
+  // beforeEach 進到每一頁之前
+  Router.beforeEach(async (to, from, next) => {
+    // 抓使用者資料
+    const user = useUserStore()
+    // 剛進網頁時的第一次路由跳轉
+    if (from === START_LOCATION) {
+      // 取得使用者資訊
+      await user.getProfile()
+    }
+    // 如果有登入且去註冊/登入頁面
+    if (user.isLogin && ['/register', '/login'].includes(to.path)) {
+      // 導向到根目錄
+      next('/')
+    } else if (to.meta.login && !user.isLogin) {
+      // 如果要去的地方要登陸，且使用者沒登入
+      // 丟到登入頁
+      next('/login')
+    } else if (to.meta.admin && !user.isAdmin) {
+      // 如果要去的地方要管理員權限，且使用者不是管理員
+      // 丟到首頁
+      next('/')
+    } else {
+      // else 該去哪就去哪，不導向
+      next()
+    }
   })
   return Router
 })
