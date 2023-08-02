29c725187c3edc9304d914bf41dbd7295ad6f500
LucienneHuang <q22641949@gmail.com>
2023-08-02 14:26:01 +0800

    刻+改 聯絡我們頁面的版面

--- src/pages/front/ContactPage.vue
+++ src/pages/front/ContactPage.vue
@@ -1,27 +1,28 @@
 <template>
-  <div id="container" class="q-ma-xl q-pa-xl">
-    <div id="head" class="q-ml-xl">
+  <div id="container" class="q-ma-md">
+    <div id="head" class="q-my-md">
       <q-breadcrumbs>
         <q-breadcrumbs-el label="Home" to="/" />
         <q-breadcrumbs-el label="聯絡" class="" />
       </q-breadcrumbs>
-    <div id="title" class=" text-h3 text-weight-bold non-selectable  q-pl-lg q-py-sm q-my-lg">聯絡</div>
+      <div id="title" class=" text-h4 text-weight-bold non-selectable q-mt-md q-pl-md">聯絡我們</div>
     </div>
     <div id="section" class="flex justify-center q-ma-xl">
       <div id="background" class="q-ma-xl">
+        <div id="backgroundtext" class="text-white text-h4 text-weight-bold">Contact Us</div>
         <q-form @submit.prevent="sendContact" class="q-ma-xl">
           <q-card class="bg-accent flex column justify-center items-center q-ma-xl">
             <q-card-section class="q-mb-sm" horizontal>
               <div class="text-h5 text-weight-bold text-white q-mr-md" >名稱</div>
-              <q-input type="text" v-model="contactForm.name" :rules="[rules.required]" dense rounded outlined bg-color="white" label="稱呼" hint="請輸入名稱" hide-hint clearable style="width: 30rem;" />
+              <q-input type="text" v-model="contactForm.name" :rules="[rules.required]" dense rounded outlined bg-color="white" label="稱呼" hint="請輸入名稱" hide-hint clearable />
             </q-card-section >
             <q-card-section class="q-mb-sm" horizontal>
               <div class="text-h5 text-weight-bold text-white q-mr-md">信箱</div>
-              <q-input type="email" v-model="contactForm.email" :rules="[rules.required,rules.email]" rounded outlined bg-color="white" label="信箱" hint="請輸入信箱" dense hide-hint clearable style="width: 30rem;" />
+              <q-input type="email" v-model="contactForm.email" :rules="[rules.required,rules.email]" rounded outlined bg-color="white" label="信箱" hint="請輸入信箱" dense hide-hint clearable />
             </q-card-section>
             <q-card-section class="q-mb-sm" horizontal>
               <div class="text-h5 text-weight-bold text-white q-mr-md">內容</div>
-              <q-input type="textarea" v-model="contactForm.message" :rules="[rules.required]" rounded outlined bg-color="white" label="內容" hint="請輸入內容" dense hide-hint clearable style="width: 30rem;" />
+              <q-input type="textarea" v-model="contactForm.message" :rules="[rules.required]" rounded outlined bg-color="white" label="內容" hint="請輸入內容" dense hide-hint clearable />
             </q-card-section>
             <q-card-actions class="q-mt-sm">
               <q-btn type="submit" unelevated rounded style="width: 6rem;" size="1rem" color="primary" label="送出" />
@@ -35,32 +36,71 @@
 <style lang="scss" scoped>
 #container{
   #background{
-    width: 48rem;
-    height: 33rem;
     background: $primary;
     border-radius: 2rem;
+    #backgroundtext{
+      position: relative;
+      top: 3rem;
+      left: 7rem;
+    }
   }
   #title{
     border-left: .8rem solid $accent;
   }
   .q-card{
-    width: 48rem;
-    height: 33rem;
+    padding: 1rem;
+    width: 22rem;
     border-radius: 2rem;
+    :deep(.q-field__control){
+      width: 12rem;
+    }
   }
 
 }
+@media(min-width:992px){
+  #container{
+    #head{
+      margin-left: 8rem;
+    }
+    #background{
+      width: 48rem;
+      height: 33rem;
+      background: $primary;
+      border-radius: 2rem;
+        #backgroundtext{
+        position: relative;
+        top: 3rem;
+        left: 7rem;
+        }
+    }
+    #title{
+      border-left: .8rem solid $accent;
+    }
+    #section{
+      margin: 6rem 6rem 12rem;
+    }
+    .q-card{
+      width: 48rem;
+      height: 33rem;
+      :deep(.q-field__control){
+        width: 30rem;
+      }
+    }
+  }
+}
 
+@media(min-width:1200px){
+}
 </style>
 <script setup>
 import { reactive } from 'vue'
 import validator from 'validator'
 import { api } from 'src/boot/axios.js'
-// import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router'
 import sweetalert from 'sweetalert2'
 import { useQuasar } from 'quasar'
 const $q = useQuasar()
-// const router = useRouter()
+const router = useRouter()
 const contactForm = reactive({
   name: '',
   email: '',
@@ -79,6 +119,7 @@ const sendContact = async () => {
       title: '成功送出',
       text: '靜待我們的回覆'
     })
+    router.push('/')
   } catch (error) {
     console.log(error)
     $q.notify({
