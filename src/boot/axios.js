1f0ed5be6c7a95f24a402192f7c558a577b47243
LucienneHuang <q22641949@gmail.com>
2023-08-02 16:38:59 +0800

    寫攔截器

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -37,7 +37,45 @@ apiAuth.interceptors.request.use(config => {
 // interceptors.response 回應攔截器
 // 1. 呼叫 axios 的 .then 跟 .catch
 // 這個處理完後才會進入到 .then .catch
-apiAuth.interceptors.response.use()
+
+// 1. getProfile
+// 2. 如果發生逾時錯誤
+// 3. 傳送舊換新請求
+// 4. 如果舊換新成功，修改 getProfile 請求的 jwt 後重新送出
+// 5. 如果舊換新失敗，將 getProfile 的錯誤回傳
+// 6. 如果不是登入逾時錯誤，將 getProfile 的錯誤回傳
+// apiAuth.interceptors.response.use(成功執行的 function, 失敗執行的 function)
+apiAuth.interceptors.response.use(res => {
+  return res
+}, error => {
+// 如果有收到回應
+  if (error.response) {
+    // 如果是 JWT 過期的錯誤，且不是舊換新請求
+    // 如果今天沒有後面那端，會一直迴圈下去
+    if (error.response.data.message === '登入逾時' && error.config !== '/users/extend') {
+      const user = useUserStore()
+      // 傳送 JWT 舊換新請求
+      return apiAuth.patch('/users/extend')
+        .then(({ data }) => {
+          // 如果舊換新成功，更新 user store 的 JWT
+          user.token = data.result
+          // 修改原本發生錯誤的請求的 JWT
+          error.config.headers.authorization = 'Bearer ' + user.token
+          // 重新傳送原請求
+          return axios(error.config)
+        })
+        .catch(() => {
+          // 如果舊換新失敗，登出
+          user.logout()
+          // 回應原錯誤到呼叫 axios 的地方
+          // 這邊要用 Promise.reject 回傳錯誤，因為 axios 他的錯誤必須要 return 一個 promise 的錯誤
+          return Promise.reject(error)
+        })
+    }
+  }
+  // 如果失敗的請求沒回應，回傳原錯誤到呼叫 axios 的地方
+  return Promise.reject(error)
+})
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
