4e6a07651d22a952f1492b6a1e4098c6cda71190
LucienneHuang <q22641949@gmail.com>
2023-08-10 16:28:28 +0800

    寫區域的版面+引入資料+圖片

--- src/pages/front/SkyRealms.vue
+++ src/pages/front/SkyRealms.vue
@@ -1,3 +1,88 @@
 <template>
-  <h2>區域</h2>
+  <div id="container" class="q-ma-md">
+    <div id="head" class="q-my-md">
+      <q-breadcrumbs>
+        <q-breadcrumbs-el label="Home" to="/" />
+        <q-breadcrumbs-el label="最新消息" />
+      </q-breadcrumbs>
+      <div id="title" class="text-weight-bold non-selectable q-mt-md q-pl-md">區域介紹</div>
+    </div>
+    <div id="section" class="flex justify-center">
+      <div v-for="realm in realms" :key="realm._id" class="q-mx-xl">
+        <RealmCard v-bind="realm"></RealmCard>
+      </div>
+    </div>
+  </div>
 </template>
+<style lang="scss" scoped>
+#container{
+  #title{
+    border-left: .8rem solid $accent;
+    font-size: 2rem;
+  }
+  #section{
+    margin: 6rem 2rem;
+  }
+}
+@media(min-width:768px){
+  #container{
+    #section{
+      margin: 6rem 0rem;
+    }
+}}
+@media(min-width:975px){
+  #container{
+    #head{
+      margin-left: 8rem;
+      #title{
+        font-size: 2.5rem;
+      }
+      .q-breadcrumbs{
+        font-size: 1.2rem;
+      }
+    }
+
+  }
+}
+@media(min-width:1200px){
+  #container{
+    #title{
+      font-size: 3rem;
+    }
+    .q-breadcrumbs{
+      font-size: 1.3rem;
+    }
+    #section{
+      margin: 6rem 3rem;
+    }
+  }
+
+}
+@media(min-width:1570px){
+  #container{
+    #section{
+      margin: 6rem 14.5rem;
+    }
+  }
+
+}
+</style>
+<script setup>
+import { ref } from 'vue'
+import { useQuasar } from 'quasar'
+import { api } from 'src/boot/axios'
+import RealmCard from 'src/components/RealmCard.vue'
+const $q = useQuasar()
+const realms = ref([]);
+(async () => {
+  try {
+    const { data } = await api.get('/articles/getRealms')
+    realms.value.push(...data.result)
+  } catch (error) {
+    $q.notify({
+      type: 'negative',
+      message: '發生錯誤'
+    })
+  }
+})()
+</script>
