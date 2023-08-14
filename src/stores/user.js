927d5aca1af2fb5e054386cc8dfae0aced910d7a
LucienneHuang <q22641949@gmail.com>
2023-08-14 16:30:00 +0800

    block

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia'
 // import { LocalStorage } from 'quasar'
 import { ref, computed } from 'vue'
 import UserRole from 'src/enums/UserRole'
+import BlockUser from 'src/enums/BlockUser'
 import { apiAuth } from 'src/boot/axios'
 // composition api 寫法
 
@@ -22,6 +23,7 @@ export const useUserStore = defineStore('user', () => {
   // 判斷管理員還是一般的使用者
   const role = ref(UserRole.USER)
   const user = ref('')
+  const block = ref(BlockUser.NO)
 
   // 寫登入的 FUNCTION
   const login = (data) => {
@@ -32,6 +34,7 @@ export const useUserStore = defineStore('user', () => {
     avatar.value = data.avatar
     cart.value = data.cart
     role.value = data.role
+    block.value = data.block
   }
   // 取得使用者資訊
   const getProfile = async () => {
@@ -46,6 +49,7 @@ export const useUserStore = defineStore('user', () => {
       avatar.value = data.result.avatar
       cart.value = data.result.cart
       role.value = data.result.role
+      block.value = data.result.block
     } catch (error) {
       // 如果 token !== 0，但又發生錯誤，可能是他的 token 過期了
       // 把他重設就好惹
@@ -60,6 +64,10 @@ export const useUserStore = defineStore('user', () => {
     return role.value === UserRole.ADMIN
   })
 
+  const isBlock = computed(() => {
+    return block.value === BlockUser.YES
+  })
+
   const logout = () => {
     user.value = ''
     token.value = ''
@@ -68,6 +76,7 @@ export const useUserStore = defineStore('user', () => {
     avatar.value = 'https://source.boringavatars.com/beam/250/unknown_user?colors=264653,2a9d8f,e9c46a,f4a261,e76f51'
     cart.value = 0
     role.value = UserRole.USER
+    block.value = BlockUser.NO
   }
   return {
     token,
@@ -76,9 +85,11 @@ export const useUserStore = defineStore('user', () => {
     avatar,
     cart,
     role,
+    block,
     login,
     isLogin,
     isAdmin,
+    isBlock,
     getProfile,
     logout,
     user
