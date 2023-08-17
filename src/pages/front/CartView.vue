f7c7f610fe61d4b334d3f093ab4ad2db97dfc074
LucienneHuang <q22641949@gmail.com>
2023-08-17 16:04:48 +0800

    寫購物車頁面

--- src/pages/front/CartView.vue
+++ src/pages/front/CartView.vue
@@ -1,4 +1,35 @@
 <template>
+  <q-dialog v-model="dialog">
+    <div id="form">
+      <q-form @submit.prevent="submit">
+        <q-card class="flex column justify-center items-center q-pa-lg q-ma-xl">
+            <!-- 姓名 -->
+          <q-card-section horizontal>
+            <div class="text-h7 q-mt-md q-mr-lg">聯絡姓名</div>
+            <q-input color="accent" type="text" label="若為交易實體商品(周邊)，請輸入真實姓名方便寄件。" v-model="submitForm.realName" :rules="[rules.required]" hide-hint/>
+          </q-card-section>
+            <!-- 電話 -->
+          <q-card-section horizontal>
+            <div class="text-h7 q-mt-md q-mr-lg">聯絡電話</div>
+            <q-input color="accent" type="text" label="請輸入連絡電話。" hide-hint v-model="submitForm.phoneNumber" :rules="[rules.required,rules.phone]"/>
+          </q-card-section >
+          <q-card-section horizontal>
+            <!-- 送貨地址 -->
+            <div class="text-h7 q-mt-md q-mr-lg">聯絡地址</div>
+            <q-input color="accent" type="text" label="請輸入送貨地址/好友連結。" hide-hint v-model="submitForm.address"/>
+          </q-card-section>
+          <q-card-section horizontal>
+            <div class="text-h7 q-mt-md q-mr-lg">付款方式</div>
+            <q-select :options="paymentOptions" color="accent" label="分類" v-model="submitForm.payment" :rules="[rules.required]"/>
+          </q-card-section>
+          <q-card-actions class="q-mt-sm">
+            <q-btn type="submit" unelevated rounded style="width: 6rem;" size="1rem" color="accent" label="送出"  v-close-popup />
+            <q-btn unelevated rounded style="width: 6rem;" size="1rem" outline color="accent" label="取消" @click="dialog=false" />
+          </q-card-actions>
+        </q-card>
+      </q-form>
+    </div>
+  </q-dialog>
   <div id="container" class="q-ma-md">
     <div id="head" class="q-my-md">
       <q-breadcrumbs>
@@ -7,75 +38,68 @@
       </q-breadcrumbs>
       <div id="title" class="text-weight-bold non-selectable q-mt-md q-pl-md">購物車</div>
     </div>
-    <div>{{ total }}</div>
     <div id="section" class="q-ma-xl">
-        <div class="row q-mx-lg">
-          <div class="col-12 col-md-9">
-            <q-markup-table v-if="carts.length===0">
+        <q-markup-table v-if="carts.length===0">
                 <thead>
                   <tr>
-                    <td colspan="6">沒有商品</td>
+                    <td colspan="7">沒有商品</td>
                   </tr>
                 </thead>
-              </q-markup-table>
-            <q-markup-table  v-for="(cart,i) in carts" :key="i" class="q-mb-xl">
-              <thead>
-                <tr>
-                  <th colspan="6"><span class="text-h6">賣家：{{ cart.seller.nickname }}（{{ cart.seller._id }}）</span></th>
-                </tr>
-                <tr>
-                  <th><span>圖片</span></th>
-                  <th class="name"><span>名稱</span></th>
-                  <th><span>單價</span></th>
-                  <th><span>數量</span></th>
-                  <th><span>小計</span></th>
-                  <th><span>操作</span></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(item,index) in cart.productCart" :key="index" class="text-center">
-                  <td>
+          </q-markup-table>
+        <q-markup-table  v-for="(cart,i) in carts" :key="i" class="q-mb-xl">
+          <thead>
+            <tr>
+              <th colspan="7"><span class="text-h6">賣家：{{ cart.seller.nickname }}（{{ cart.seller._id }}）</span></th>
+            </tr>
+            <tr>
+              <th></th>
+              <th><span>圖片</span></th>
+              <th class="name"><span>名稱</span></th>
+              <th><span>單價</span></th>
+              <th><span>數量</span></th>
+              <th><span>小計</span></th>
+              <th><span>操作</span></th>
+            </tr>
+          </thead>
+          <tbody>
+              <tr v-for="(item,index) in cart.productCart" :key="index" class="text-center">
+                <td><span v-if="!item.product.sell"><q-icon  name="mdi-alert" color="red" size="25px"/><br>已下架</span></td>
+                <td class="image">
                     <q-img
                     :src="item.product.image"
                     style="height: 140px;width: 150px"
                   />
-                  </td>
-                  <td class="name">{{ item.product.name }}</td>
-                  <td>
+                </td>
+                <td class="name"><router-link :to="'/trade/'+item.product._id">{{ item.product.name }}</router-link></td>
+                <td>
                     {{ item.product.price }}
                   </td>
-                  <td>
-                    <q-btn round color="accent" icon="mdi-plus" flat @click="updateCart(cart.seller._id,item.product._id,1)"/>
-                    {{ item.quantity }}
-                    <q-btn round color="accent" icon="mdi-minus" flat @click="updateCart(cart.seller._id,item.product._id,-1)"/>
-                  </td>
-                  <td>{{ item.quantity * item.product.price }}</td>
-                  <td>
+                <td>
+                  <q-btn round color="accent" icon="mdi-minus" flat @click="updateCart(cart.seller._id,item.product._id,-1)"/>
+                  {{ item.quantity }}
+                  <q-btn round color="accent" icon="mdi-plus" flat @click="updateCart(cart.seller._id,item.product._id,1)"/>
+                </td>
+                <td>{{ item.quantity * item.product.price }}</td>
+                <td>
                     <q-btn round color="accent" icon="mdi-delete"  @click="updateCart(cart.seller._id,item.product._id,item.quantity * -1)"/>
-                  </td>
-                </tr>
-              </tbody>
-            </q-markup-table>
-          </div>
-          <div class=" col-12 col-md-3 flex justify-center">
-            <div class="price">
-              <!-- test -->
-              <q-btn color="primary" label="Primary" @click="test" :disable="!canCheckout" />
-            </div>
-          </div>
+                </td>
+              </tr>
+          </tbody>
+        </q-markup-table>
+        <div class="price">
+          <hr>
+          <div class="text-h4 text-weight-bold q-pt-sm non-selectable">商品總金額：<span class="text-red-10">{{ total }}</span></div>
+          <q-btn color="accent" size="18px" label="前往結帳" @click="checkOut" :disable="!canCheckout" />
         </div>
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
 .price{
-  width: 90%;
-  height: 400px;
-  background-color: red;
-  max-width: 320px;
-}
-.name{
-  width: 200px;
+  text-align: center;
+  line-height: 70px;
+  width: 100%;
+
 }
 #container{
   #title{
@@ -84,6 +108,32 @@
   }
   thead tr:nth-child(2) span{
     font-size: 1rem;
+  }
+}
+#form{
+    width: 35rem;
+    max-width: 100vw;
+    .q-card{
+    border: 4px solid $accent;
+    width: 25rem;
+    border-radius: 2rem;
+    :deep(.q-field__control){
+      width: 12rem;
+    }
+  }
+}
+tbody{
+  tr {
+    & > :nth-child(1){
+      width: 84px;
+    }
+    & > :nth-child(2){
+      width: 150px;
+    }
+    & > :nth-child(3){
+      width: 230px;
+    }
+
   }
 }
 @media(min-width:975px){
@@ -97,36 +147,69 @@
         font-size: 1.2rem;
       }
     }
-    #background{
-      width: 48rem;
-      height: 33rem;
-    }
     #section{
-      margin: 6rem 6rem 12rem;
+      margin: 5rem 6rem 5rem;
+    }
+  }
+  #form{
+    width: 50rem;
+    max-width: 100vw;
+    .q-card{
+      font-size: 1rem;
+      width: 40rem;
+      :deep(.q-field__control){
+        width: 30rem;
+      }
     }
   }
 }
 
 @media(min-width:1200px){
-  #title{
-    font-size: 3rem;
-  }
-  .q-breadcrumbs{
-    font-size: 1.3rem;
+  #form{
+    width: 60rem;
+    max-width: 100vw;
+    .q-card{
+      font-size: 1rem;
+      width: 50rem;
+      :deep(.q-field__control){
+        width: 40rem;
+      }
+      .q-editor{
+        width: 40rem;
+      }
+    }
   }
 }
 </style>
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, reactive } from 'vue'
 import { useQuasar } from 'quasar'
+import { useRouter } from 'vue-router'
+import validator from 'validator'
 import sweetalert from 'sweetalert2'
 import 'animate.css'
 import { apiAuth } from 'src/boot/axios'
+const router = useRouter()
 const $q = useQuasar()
 const carts = ref([])
+const dialog = ref(false)
+// 付款方式
+const paymentOptions = ['轉帳', 'linepay', '貨到付款']
+// 規範
+const rules = {
+  required: (value) => !!value || '欄位必填',
+  phone: (value) => validator.isMobilePhone(value, 'zh-TW') || '手機格式錯誤',
+  paymentRequired: (value) => paymentOptions.includes(value) || '沒有該分類'
+}
 
+const submitForm = reactive({
+  realName: '',
+  phoneNumber: '',
+  address: '',
+  payment: ''
+})
+// 更新購物車
 const updateCart = async (seller, product, quantity) => {
-  console.log(seller, product, quantity)
   try {
     await apiAuth.post('/users/cart', {
       seller,
@@ -142,9 +225,43 @@ const updateCart = async (seller, product, quantity) => {
     if (carts.value[idxSeller].productCart.length <= 0) {
       carts.value.splice(idxSeller, 1)
     }
+    $q.notify({
+      type: 'positive',
+      message: '更新成功'
+    })
+  } catch (error) {
+    $q.notify({
+      type: 'negative',
+      message: error.response.data.message
+    })
+  }
+}
+// 實時更新商品總金額
+const total = computed(() => {
+  return carts.value.reduce((total, cart) => {
+    const sellerPrice = cart.productCart.reduce((ownSum, item) => {
+      return ownSum + (item.quantity * item.product.price)
+    }, 0)
+    return total + sellerPrice
+  }, 0)
+})
+// 實時確認商品是否可以結帳
+const canCheckout = computed(() => {
+  // 每個賣家的購物車去檢查
+  // 如果最終回傳結果是 false (商品都有上架)，則 !後變成 true，反之則 false
+  return carts.value.length > 0 && !carts.value.some(cart => {
+    // 檢查每個賣家的購物車內的商品
+    // 如果商品有一個沒有上架，回傳 true
+    // 如果商品都有上架，回傳 false
+    return cart.productCart.some(product => !product.product.sell)
+  })
+})
+const submit = async (req, res) => {
+  try {
+    await apiAuth.post('/orders', submitForm)
     await sweetalert.fire({
       icon: 'success',
-      title: '更新購物車成功',
+      title: '訂單建立成功',
       showClass: {
         popup: 'animate__animated animate__fadeInDown'
       },
@@ -155,6 +272,7 @@ const updateCart = async (seller, product, quantity) => {
       confirmButtonColor: '#B0A9EC',
       width: '20rem'
     })
+    router.push('/member/orders')
   } catch (error) {
     $q.notify({
       type: 'negative',
@@ -162,24 +280,10 @@ const updateCart = async (seller, product, quantity) => {
     })
   }
 }
-const total = computed(() => {
-  return carts.value.reduce((total, cart) => {
-    const sellerPrice = cart.productCart.reduce((ownSum, item) => {
-      return ownSum + (item.quantity * item.product.price)
-    }, 0)
-    return total + sellerPrice
-  }, 0)
-})
-const canCheckout = computed(() => {
-  // 每個賣家的購物車去檢查
-  // 如果最終回傳結果是 false (商品都有上架)，則 !後變成 true，反之則 false
-  return carts.value.length > 0 && !carts.value.some(cart => {
-    // 檢查每個賣家的購物車內的商品
-    // 如果商品有一個沒有上架，回傳 true
-    // 如果商品都有上架，回傳 false
-    return cart.productCart.some(product => !product.product.sell)
-  })
-});
+const checkOut = () => {
+  dialog.value = true
+};
+// 取得購物車資訊
 (async () => {
   try {
     const { data } = await apiAuth.get('/users/cart')
@@ -188,7 +292,4 @@ const canCheckout = computed(() => {
 
   }
 })()
-const test = () => {
-  console.log('可以結帳')
-}
 </script>
