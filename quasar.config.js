a09a46ce0fd8890596c9c50b36383a6dcc0f0d69
LucienneHuang <q22641949@gmail.com>
2023-08-21 13:12:23 +0800

    test

--- quasar.config.js
+++ quasar.config.js
@@ -65,6 +65,7 @@ module.exports = configure(function (/* ctx */) {
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
       publicPath: '/sky_front',
+
       // analyze: true,
       env: {
         VITE_API: process.env.VITE_API
