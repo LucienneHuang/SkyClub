51e65c08157ab2ca330ecaad6191f417f907bc75
LucienneHuang <119398302+LucienneHuang@users.noreply.github.com>
2023-08-23 03:55:17 +0000

    Deploying to gh-pages from @ LucienneHuang/SkyClub@514aba56618ceacc909a816b5c45a2a772402828 🚀

---
+++ assets/use-quasar.e36b9b6f.js
@@ -0,0 +1,1 @@
+import{p as a,ao as r}from"./index.6b2ef3fc.js";function u(){return a(r)}export{u};
---
+++ assets/use-ratio.361238d7.js
@@ -0,0 +1,1 @@
+import{u}from"./index.6b2ef3fc.js";const e={ratio:[String,Number]};function a(r,t){return u(()=>{const o=Number(r.ratio||(t!==void 0?t.value:void 0));return isNaN(o)!==!0&&o>0?{paddingBottom:`${100/o}%`}:null})}export{a,e as u};
