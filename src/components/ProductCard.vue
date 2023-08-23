514aba56618ceacc909a816b5c45a2a772402828
LucienneHuang <q22641949@gmail.com>
2023-08-23 11:54:38 +0800

    +router link

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -7,7 +7,10 @@
       </router-link>
       <q-card-section>
         <div class="text-weight-bold">賣家：{{ user.nickname }}<br>&nbsp;ID:&nbsp;：{{user._id}}</div>
-        <div class="text-h5 text-weight-bolder text-center q-mt-sm">{{ name }}</div>
+        <div class="text-h5 text-weight-bolder text-center q-mt-sm">
+          <router-link :to="{path:'/trade/'+ _id}">{{ name }}
+          </router-link>
+        </div>
         <div class="text-center text-weight-bolder text-red-10" style="font-size: 1.3rem;">{{ price }}&nbsp;{{ currency }}</div>
         <q-chip v-if="category==='季票'" color="pink-3" text-color="white" icon="mdi-tag">{{category}}</q-chip>
         <q-chip v-else-if="category==='周邊'" color="deep-purple-4
