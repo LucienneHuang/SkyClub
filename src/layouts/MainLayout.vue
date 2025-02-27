c3c500882ada0ad989ecc357417c74e05fe80165
q22641949@gmail.com <q22641949@gmail.com>
2025-02-27 22:42:28 +0800

    revert component extraction

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -67,6 +67,24 @@
       <div class="deco"></div>
     </q-header>
     <!-- 手機螢幕才會有的側邊欄 -->
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered behavior="mobile" :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-white'">
+      <!-- 之後藥補的側邊攔內容 -->
+      <q-scroll-area class="fit">
+          <q-list>
+            <template v-for="(navItem, index) in navList" :key="navItem.to">
+              <q-item  clickable :active="navItem.label === 'Outbox'" v-ripple :to="navItem.to">
+                <q-item-section avatar>
+                  <q-icon :name="navItem.icon" />
+                </q-item-section>
+                <q-item-section>
+                  {{ navItem.label }}
+                </q-item-section>
+              </q-item>
+              <q-separator :key="'sep' + index"  v-if="navItem.separator" />
+            </template>
+          </q-list>
+        </q-scroll-area>
+    </q-drawer>
     <ScrollToTop/>
     <MainFooter/>
   </q-layout>
@@ -94,6 +112,34 @@ const leftDrawerOpen = ref(false)
 function toggleLeftDrawer () {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
+// navbar 的字 & 路徑 & icon
+const navList = [
+  {
+    to: '/',
+    label: '首頁',
+    icon: 'home'
+  },
+  {
+    to: '/latestnews',
+    label: '最新消息',
+    icon: 'announcement'
+  },
+  {
+    to: '/realms',
+    label: '區域介紹',
+    icon: 'location_on'
+  },
+  {
+    to: '/trade',
+    label: '交易專區',
+    icon: 'local_mall'
+  },
+  {
+    to: '/contact',
+    label: '聯絡我們',
+    icon: 'call'
+  }
+]
 
 // 最右邊頭貼 menu 的文字 & 路徑 & 是否顯示
 const menuList = computed(() => {
